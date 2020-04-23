96adc8fb4418a4c78a3762607bbb8a3742542ae5
Pedro Lauro <pedro.lauro@tarea.com.br>
2020-04-23 20:33:34 -0300

    executando a partir da pasta

--- index.js
+++ index.js
@@ -1,51 +1,85 @@
 'use strict'
 
+const chalk = require('chalk')
 const path = require('path')
 const async = require('async')
 const newman = require('newman')
-const moment = require('moment-timezone');
+const moment = require('moment-timezone')
+const fs = require('fs')
 
-// PASTA PARA RESULTADOS
-const NOW = moment().tz("America/Sao_Paulo").format('YYYY_MM_DD_HH_mm_ss')
-console.log(`\n\nRESULTADOS ESTARÃO EM executions/${NOW}`)
 
-// LISTA DE UFS
-var UFS = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
-// const UFS = ['AC', 'AP']
+/**
+ * VARS
+ */
+const NOW = moment().tz('America/Sao_Paulo').format('YYYY_MM_DD_HH_mm_ss')
+const UFS = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
+const S3_FOLDER = 's3/20200421'
+const DOC_FOLDER = `./postman/docs/${S3_FOLDER}`
+const OUTPUT_FOLDER = `executions/${NOW}`
+const FILE_PATTERN = /([A-Z]{2})_01\.json/
+const BARS = `######################################################`
 
-const parameters = (uf) => ({
-    collection: path.join(__dirname, 'postman/carga_docs.postman_collection.json'),
-    environment: path.join(__dirname, `postman/env/prd/${uf}.json`),
-    iterationData: path.join(__dirname, `postman/docs/s3/20200421/${uf}_01.json`),
+/**
+ * FUNCS
+ */
+const joinPath = (filePath) => path.join(__dirname, filePath)
+
+const createNewRunCmd = (uf, dataPath) => (done) => {
+    let param = runParamByUf(uf, dataPath)
+    newman.run(param, done)
+}
+
+const runParamByUf = (uf, dataPath) => ({
+    id: uf,
+    collection: joinPath('postman/carga_docs.postman_collection.json'),
+    environment: joinPath(`postman/env/prd/${uf}.json`),
+    iterationData: joinPath(dataPath),
     iterationCount: 1,
     reporters: ['json'],
     reporter: {
         json: {
-            export: path.join(__dirname, `executions/${NOW}/output.${uf}.json`)
+            export: joinPath(`executions/${NOW}/output.${uf}.json`)
         }
-    },
-    id: uf
+    }
 })
-
-const parallelCollectionRun = (uf) => (done) => {
-    let param = parameters(uf)
-    // console.log(`params, uf=${uf}, param.id=${param.id}`)
-    newman.run(param, done)
-};
-
-let commands = []
-for (let index = 0; index < UFS.length; index++) {
-    commands.push(parallelCollectionRun(UFS[index]))
-}
 
 // waiting para debug
 // console.log(`\n\nWAITING\n\n`)
 setTimeout(run, 100)
 
 function run() {
-    console.log(`\n\n#########################################`)
-    console.log(`INICIO DA EXECUCAO`)
-    console.log(`#########################################\n\n`)
+
+    console.log(chalk.magenta(`\n\n${BARS}`))
+    console.log(chalk.cyan(`INICIO DA EXECUCAO`))
+    console.log(chalk.cyan(`DOCUMENTOS DA PASTA `) + chalk.yellow(S3_FOLDER))
+    console.log(chalk.cyan(`RESULTADOS EM `) + chalk.yellow(OUTPUT_FOLDER))
+    console.log(chalk.magenta(`${BARS}\n\n`))
+
+    let commands = []
+    let falha = false
+    fs.readdir(DOC_FOLDER, (err, files) => {
+        files.forEach(file => {
+
+            if (falha) {
+                return
+            }
+
+            if (!file.match(FILE_PATTERN)) {
+                console.log(chalk.redBright(`ARQUIVO INVALIDO: `) +
+                    chalk.bold.redBright(`${DOC_FOLDER}/${file}\n\n`))
+                falha = true
+                return
+            }
+
+            let uf = file.replace(FILE_PATTERN, '$1')
+            // console.log(`uf=${uf}, path=${file}`)
+            commands.push(createNewRunCmd(uf, `${DOC_FOLDER}/${file}`))
+        })
+
+        if (falha) {
+            return
+        }
+    })
 
     // Runs the Postman sample collection thrice, in parallel.
     async.parallel(
@@ -62,16 +96,16 @@ function run() {
                 let failures = result.run.stats.assertions.failed
                 totalGeral += total
                 failuresGeral += failures
-                
+
                 let falhasTxt = `${failures} (${100*failures/total}%)`
                 console.log(`${name}, host=${host}, total=${total}, falhas=${falhasTxt}`)
             })
-            
+
             let falhasGeralTxt = `${failuresGeral} (${100*failuresGeral/totalGeral}%)`
             console.log(`\nGERAL: total=${totalGeral}, falhas=${falhasGeralTxt}`)
-            console.log(`\n\n#########################################`)
+            console.log(`\n\n${BARS}`)
             console.log(`TERMINO DA EXECUCAO`)
-            console.log(`#########################################\n\n`)
+            console.log(`${BARS}\n\n`)
         }
     )
 }
