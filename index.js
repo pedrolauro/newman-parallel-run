d683d6413bbdc7d3ae4659edd7fd05ce7aea3093
Pedro Lauro <pedro.lauro@tarea.com.br>
2020-04-23 16:11:42 -0300

    execucao de todos os estados

--- index.js
+++ index.js
@@ -1,52 +1,74 @@
 const path = require('path')
 const async = require('async')
 const newman = require('newman')
+var moment = require('moment-timezone');
 
-const PARALLEL_RUN_COUNT = 2
+// PASTA PARA RESULTADOS
+var NOW = moment().tz("America/Sao_Paulo").format('YYYY_MM_DD_HH_mm_ss')
+console.log(`\n\nRESULTADOS ESTARÃO EM executions/${NOW}`)
 
-const parametersForTestRun1 = {
-    collection: path.join(__dirname, 'postman/carga_docs.postman_collection.json'), // your collection
-    environment: path.join(__dirname, 'postman/prd.AL.postman_environment.json'), //your env
-    iterationData: path.join(__dirname, 'postman/docs.AL.json')
-    ,reporters: ['json']
-    ,reporter: { json: { export: path.join(__dirname, 'executions/output.AL.json') } }
-    ,id: 'AL'
-};
+// LISTA DE UFS
+// var UFS = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
+var UFS = ['AL', 'DF']
 
-const parametersForTestRun2 = {
-    collection: path.join(__dirname, 'postman/carga_docs.postman_collection.json'), // your collection
-    environment: path.join(__dirname, 'postman/prd.DF.postman_environment.json'), //your env
-    iterationData: path.join(__dirname, 'postman/docs.DF.json')
-    ,reporters: ['json']
-    ,reporter: { json: { export: path.join(__dirname, 'executions/output.DF.json') } }
-    ,id: 'DF'
-};
+const parameters = (uf) => ({
+    collection: path.join(__dirname, 'postman/carga_docs.postman_collection.json'),
+    environment: path.join(__dirname, `postman/prd.${uf}.postman_environment.json`),
+    iterationData: path.join(__dirname, `postman/docs.${uf}.json`),
+    reporters: ['json'],
+    reporter: {
+        json: {
+            export: path.join(__dirname, `executions/${NOW}/output.${uf}.json`)
+        }
+    },
+    id: uf
+})
 
-parallelCollectionRun = (i) => (done) => {
-    
-    let param = parametersForTestRun1
-    if (i === 1) {
-        param = parametersForTestRun2
-    }
-    
-    console.log(`params, i=${i}, param.id=${param.id}`)
-    newman.run(param, done);
+parallelCollectionRun = (uf) => (done) => {
+    let param = parameters(uf)
+    // console.log(`params, uf=${uf}, param.id=${param.id}`)
+    newman.run(param, done)
 };
 
 let commands = []
-for (let index = 0; index < PARALLEL_RUN_COUNT; index++) {
-    commands.push(parallelCollectionRun(index));
+for (let index = 0; index < UFS.length; index++) {
+    commands.push(parallelCollectionRun(UFS[index]))
 }
 
-// Runs the Postman sample collection thrice, in parallel.
-async.parallel(
-    commands,
-    (err, results) => {
-        err && console.error(err);
+// waiting para debug
+// console.log(`\n\nWAITING\n\n`)
+setTimeout(run, 100)
 
-        results.forEach(function (result) {
-            var failures = result.run.failures;
-            console.info(failures.length ? JSON.stringify(failures.failures, null, 2) :
-                `${result.collection.name} ran successfully.`);
-        });
-    });
+function run() {
+    console.log(`\n\n#########################################`)
+    console.log(`INICIO DA EXECUCAO`)
+    console.log(`#########################################\n\n`)
+
+    // Runs the Postman sample collection thrice, in parallel.
+    async.parallel(
+        commands,
+        (err, results) => {
+            err && console.error(err)
+
+            var totalGeral = 0
+            var failuresGeral = 0
+            results.forEach(function (result) {
+                let name = result.environment.name
+                let host = result.environment.values.filter(i => i.key === 'host')[0].value
+                let total = result.run.stats.assertions.total
+                let failures = result.run.stats.assertions.failed
+                let falhas = `${failures} (${100*failures/total}%)`
+                
+                totalGeral += total
+                failuresGeral += failures
+                console.log(`${name}, host=${host}, total=${total}, falhas=${falhas}`)
+            })
+            
+            falhas = `${failuresGeral} (${100*failuresGeral/totalGeral}%)`
+            console.log(`\nGERAL: total=${totalGeral}, falhas=${falhas}`)
+            console.log(`\n\n#########################################`)
+            console.log(`TERMINO DA EXECUCAO`)
+            console.log(`#########################################\n\n`)
+        }
+    )
+}
